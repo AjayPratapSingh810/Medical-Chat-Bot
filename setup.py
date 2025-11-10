c63692745f0e51ab34c347f104b88e1ae7358a18
ajay pratap singh <pratapaj810@gmail.com>
2025-11-10 08:19:02 +0000

    add requirements and setup in project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Ajay Pratap singh",
+    author_email="pratapaj810@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
